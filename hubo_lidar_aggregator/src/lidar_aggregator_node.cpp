061eef1acf8cc6cc5985e6efa4d029e6dada6436
Andrew Price <arprice@gatech.edu>
2013-11-17 19:34:19 -0500

    Added ability to filter lidar scan by intensity and distance.

--- hubo_lidar_aggregator/src/lidar_aggregator_node.cpp
+++ hubo_lidar_aggregator/src/lidar_aggregator_node.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <std_msgs/Bool.h>
 #include <math.h>
 #include <string>
 #include <tf/tf.h>
@@ -13,16 +14,151 @@
 #include <pcl/ros/conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/io/io.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+
+#include <numeric>
 
 #include <algorithm>
 #include <vector>
 #include <Eigen/Dense>
 
+inline double max(const double a, const double b)
+{
+	return (a > b) ? a : b;
+}
+
+inline double min(const double a, const double b)
+{
+	return (a < b) ? a : b;
+}
+
+struct FilterSettings
+{
+	bool enableFilter;
+	double intensityMin;
+	double intensityMax;
+	double intensityStdDevs;
+	double distanceStdDevs;
+	int numNeighbors;
+
+	FilterSettings()
+	{
+		enableFilter = true;
+		intensityMin = -std::numeric_limits<double>::max();
+		intensityMax = std::numeric_limits<double>::max();
+		intensityStdDevs = 2;
+		distanceStdDevs = 2;
+		numNeighbors = 20;
+	}
+};
+
+std::ostream& operator <<(std::ostream& s, FilterSettings& i)
+{
+	s << "intensityMin: " << i.intensityMin << "\t";
+	s << "intensityMax: " << i.intensityMax << "\t";
+	s << "intensityStdDevs: " << i.intensityStdDevs << "\t";
+	s << "distanceStdDevs: " << i.distanceStdDevs << "\t";
+
+	return s;
+}
+
+struct IntensityStats
+{
+	double mean;
+	double stdDev;
+	double min;
+	double max;
+	//double median;
+
+	IntensityStats()
+	{
+		max = -std::numeric_limits<double>::max();
+		min = std::numeric_limits<double>::max();
+		mean = 0.0;
+		stdDev = 0.0;
+		//median = 0.0;
+	}
+
+	typedef boost::shared_ptr<IntensityStats> Ptr;
+};
+
+std::ostream& operator <<(std::ostream& s, IntensityStats& i)
+{
+	s << "max: " << i.max << "\t";
+	s << "min: " << i.min << "\t";
+	s << "mean: " << i.mean << "\t";
+	s << "stdDev: " << i.stdDev << "\t";
+
+	return s;
+}
+
+
+IntensityStats::Ptr getStats(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud)
+{
+	IntensityStats::Ptr stats(new IntensityStats);
+
+	for (int i = 0; i < cloud->points.size(); ++i)
+	{
+		//pcl::PointXYZI& p = cloud->points[i];
+		float intensity = cloud->points[i].intensity;
+		stats->mean += intensity;
+
+		if (intensity > stats->max) { stats->max = intensity; }
+		if (intensity < stats->min) { stats->min = intensity; }
+
+	}
+
+	stats->mean /= cloud->points.size();
+
+	// TODO: more efficient implementation of this...
+	for (int i = 0; i < cloud->points.size(); ++i)
+	{
+		float intensity = cloud->points[i].intensity;
+		double delta = stats->mean - intensity;
+		stats->stdDev += delta * delta;
+	}
+
+	stats->stdDev /= cloud->points.size();
+	stats->stdDev = sqrt(stats->stdDev);
+
+	return stats;
+}
+
+
+FilterSettings g_settings;
+
 std::string g_fixed_frame;
-ros::Publisher g_cloud_publisher;
+//ros::Publisher g_cloud_publisher;
+ros::Subscriber g_filter_enable_sub;
 laser_geometry::LaserProjection g_laser_projector;
 tf::TransformListener* g_transformer;
+
+void cleanPointCloud(sensor_msgs::PointCloud2& cloudIn)
+{
+	pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+	pcl::fromROSMsg(cloudIn, *cloud);
+
+	IntensityStats::Ptr stats = getStats(cloud);
+
+	pcl::PassThrough<pcl::PointXYZI> pass;
+	pass.setInputCloud (cloud);
+	pass.setKeepOrganized(true);
+	pass.setFilterFieldName ("intensity");
+	pass.setFilterLimits (max(g_settings.intensityMin, stats->mean - (g_settings.intensityStdDevs*stats->stdDev)),
+						  min(g_settings.intensityMax, stats->mean + (g_settings.intensityStdDevs*stats->stdDev)));
+	//pass.setFilterLimitsNegative (true);
+	pass.filter (*cloud);
+
+	pcl::StatisticalOutlierRemoval<pcl::PointXYZI> sor;
+	sor.setInputCloud (cloud);
+	sor.setKeepOrganized(true);
+	sor.setMeanK (g_settings.numNeighbors);
+	sor.setStddevMulThresh (g_settings.distanceStdDevs);
+	sor.filter (*cloud);
+
+	pcl::toROSMsg(*cloud, cloudIn);
+}
 
 bool TimeLess(const sensor_msgs::LaserScan& s1, const sensor_msgs::LaserScan& s2) {
 	return s1.header.stamp.toSec() < s2.header.stamp.toSec();
@@ -61,9 +197,9 @@ double ComputeTiltVelocity(const std::vector<dynamixel_msgs::JointState>& tilts)
 bool LaserAggregationServiceCB(hubo_sensor_msgs::LidarAggregation::Request& req, hubo_sensor_msgs::LidarAggregation::Response& res)
 {
     ROS_INFO("Attempting to aggregate %ld laser scans into a pointcloud", req.Scans.size());
-	g_transformer->waitForTransform("/head_base_link", ros::Time::now(),
+	g_transformer->waitForTransform(g_fixed_frame, ros::Time::now(),
 									"/lidar_optical_frame", ros::Time::now(),
-									"/head_base_link", ros::Duration(5.0));
+									g_fixed_frame, ros::Duration(5.0));
     sensor_msgs::PointCloud2 full_cloud;
     if (req.Scans.size() > 0)
     {
@@ -93,6 +229,12 @@ bool LaserAggregationServiceCB(hubo_sensor_msgs::LidarAggregation::Request& req,
 	        full_cloud.width = full_cloud.width/full_cloud.height; // Doing this right prevents malloc segfaults in rviz, otherwise leave it unorganized
         }
         ROS_INFO("Created cloud of width: %d, height: %d.", full_cloud.width, full_cloud.height);
+
+		// Clean up messy lidar results
+		if (g_settings.enableFilter)
+		{
+			cleanPointCloud(full_cloud);
+		}
     }
     else
     {
@@ -100,6 +242,7 @@ bool LaserAggregationServiceCB(hubo_sensor_msgs::LidarAggregation::Request& req,
     }
     full_cloud.header.frame_id = g_fixed_frame;
     full_cloud.header.stamp = ros::Time::now();
+
     res.header.frame_id = g_fixed_frame;
     res.header.stamp = full_cloud.header.stamp;
     res.Cloud = full_cloud;
@@ -107,22 +250,35 @@ bool LaserAggregationServiceCB(hubo_sensor_msgs::LidarAggregation::Request& req,
     return true;
 }
 
+void filterEnableCB(const std_msgs::BoolConstPtr& on)
+{
+	g_settings.enableFilter = on->data;
+}
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "lidar_aggregator");
     ros::NodeHandle nh;
     ros::NodeHandle nhp("~");
+
+	nhp.getParam("intensity_floor", g_settings.intensityMin);
+	nhp.getParam("intensity_ceiling", g_settings.intensityMax);
+	nhp.getParam("intensity_stddevs", g_settings.intensityStdDevs);
+	nhp.getParam("distance_stddevs", g_settings.distanceStdDevs);
+	nhp.getParam("num_neighbors", g_settings.numNeighbors);
+
+	g_filter_enable_sub = nh.subscribe("enable_filter", 1, filterEnableCB);
+
     tf::TransformListener listener(nh, ros::Duration(20000.0));
     g_transformer = &listener;
     ROS_INFO("Starting LIDAR aggregator...");
     nhp.param(std::string("fixed_frame"), g_fixed_frame, std::string("/torso_lift_link"));
-    g_cloud_publisher = nh.advertise<sensor_msgs::PointCloud2>("pointcloud", 1, true);
+	//g_cloud_publisher = nh.advertise<sensor_msgs::PointCloud2>("pointcloud", 1, true);
     ros::ServiceServer server = nh.advertiseService("aggregate_lidar", LaserAggregationServiceCB);
     ROS_INFO("LIDAR aggregator loaded");
-    while (ros::ok())
-    {
-        ros::spinOnce();
-    }
+
+	ros::spin();
+
     ROS_INFO("Shutting down LIDAR aggregator");
     return 0;
 }
